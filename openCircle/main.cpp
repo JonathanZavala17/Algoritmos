b717d03a7557a6931f6baf63f24f4fd50997b205
rooster17 <rooster17@debian>
2018-03-07 16:14:35 -0600

    pro

---
+++ openCircle/main.cpp
@@ -0,0 +1,42 @@
+#include <GL/gl.h>
+#include <GL/glut.h>
+#include <math.h>
+
+void display(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glPointSize(2);
+    glColor3f(1.0f, 1.0f, 1.0f);
+    GLfloat angulo;
+    int i;
+    
+    glBegin(GL_LINES);
+    for (i=0; i<360; i+=1)
+    {
+		angulo = i*M_PI/180.0f; //grados a radianes
+		glVertex3f(0.0f,0.0f,0.0f);
+		glVertex3f(cos(angulo),sin(angulo),0.0f);
+		}
+    glEnd();
+    glFlush ();
+
+}
+void init (void)
+{
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize (250, 250);
+    glutInitWindowPosition (100, 100);
+    glutCreateWindow ("Ejemplo Puntos");
+    init ();
+    glutDisplayFunc(display);
+    glutMainLoop();
+    return 0;
+}
